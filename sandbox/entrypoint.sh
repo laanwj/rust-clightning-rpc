5267942e7ed8f8d47a8a33f8d83d442be9bd5a22
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-06-09 00:53:29 +0200

    github actions 2: Improve scripts to run the snadbox
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -2,6 +2,7 @@
 ./run-bitcoin.sh
 ./generate-block-bitcoin.sh
 ./run-clightning.sh
+sleep 0.5m # Give the time to c-lightning to sync the network
 cd code
 echo "Running test with Cargo"
 RESULT_TEST=$(cargo test)
@@ -9,7 +10,7 @@ cd ..
 ./stop-clightning.sh
 ./stop-bitcoin.sh
 rm -rf **/*.pid **/regtest
-
+echo "Result test is $RESULT_TEST"
 if [ "$RESULT_TEST" -eq "0" ] ; then
     exit 0
 else
